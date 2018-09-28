8960670f5c2bcc582c04981e3023c03c71ae792f
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-09-28 14:33:11 +0530

    Addtional Tests

--- test/appTest.js
+++ test/appTest.js
@@ -3,22 +3,34 @@
 
 const assert = require('chai').assert; 
 
-const sayHello = require('../app').sayHello;
+// const sayHello = require('../app').sayHello;
 
-// const app = require('../app').sayHello;
+// const addNumbers = require('../app').addNumbers
+
+const app = require('../app');
 
 // Create Tests 
 
 describe('App' , function(){
     it('app should return Hello Tester!' , function(){
-        let result = sayHello()
+        let result = app.sayHello()
         // app() --> Parentheses added as app Returns a function 
         assert.equal(result, 'Hello Tester!' );
     })
 
     it("SayHello Should return type String" , function(){
-        let result = sayHello()
+        let result = app.sayHello()
         assert.typeOf(result , 'string');
+    })
+
+    it("AddNumbers should return value above 5" , function(){
+        let result = app.addNumbers(1,5)
+        assert.isAbove(result , 5 )
+    })
+
+    it("AddNumbers Should return type Number" , function(){
+        let result = app.addNumbers()
+        assert.typeOf(result , 'number')
     })
 })
 
