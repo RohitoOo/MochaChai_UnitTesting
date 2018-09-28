77bec885c3633d3c82ef6ec5fb1e331d85c7ae78
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-09-28 14:08:46 +0530

    Unit Testing with Mocha and Chai - JavaScript

---
+++ test/appTest.js
@@ -0,0 +1,22 @@
+// Default NodeJs Assertion Library 
+// const assert = require('assert ')
+
+const assert = require('chai').assert; 
+
+const sayHello = require('../app').sayHello;
+
+// const app = require('../app').sayHello;
+
+// Create Tests 
+
+describe('App' , function(){
+    it('app should return Hello Tester!' , function(){
+        let result = sayHello()
+        // app() --> Parentheses added as app Returns a function 
+        assert.equal(result, 'Hello Tester!' );
+    })
+})
+
+// npm run test -s OR Add " mocha || true " in package.json to Remove 
+// additional Error Messages below result of testing 
+
